a6f645624b230c7894cb70dbf00939d7992ea4fe
Christoph Thiede <christoph.thiede@student.hpi.de>
2022-11-17 20:43:40 +0100

    wip p1
    Co-authored-by: Benito <benbuc@users.noreply.github.com>

--- Groups/benito-christoph-ivan/p1.py
+++ Groups/benito-christoph-ivan/p1.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 import pandas as pd
 import numpy as np
 import typing
+from tqdm import tqdm
 
 
 def load_transitions():
@@ -122,7 +123,9 @@ def random_walk(transitions, num_steps):
 	yield (current_component, current_state)
 	for _ in range(num_steps):
 		if transitions.loc[current_component].sum() == 0:
-			break
+			# break
+			yield (current_component, current_state)
+			continue
 
 		name, trace_pattern = TracePattern.random_applicable(TRACE_PATTERNS, (current_component, current_state))
 		trace = trace_pattern.apply((current_component, current_state), transitions)
@@ -130,16 +133,112 @@ def random_walk(transitions, num_steps):
 		(current_component, current_state) = trace[-1]
 
 def random_walks(transitions, num_walks, num_steps):
-	for _ in range(num_walks):
+	for _ in tqdm(range(num_walks), desc='Generating random walks'):
 		yield list(random_walk(transitions, num_steps))
 
-def main():
+def estimate_transition_matrix(transition_logs, components):
+	compound_states = [(component, state) for component in components for state in STATES]
+	transition_matrix = pd.DataFrame(0, index=compound_states, columns=compound_states)
+	for log in tqdm(transition_logs, desc='Estimating transition matrix'):
+		for compound, next_compound in zip(log, log[1:]):
+			transition_matrix.loc[[compound], [next_compound]] += 1
+	transition_matrix = transition_matrix.div(transition_matrix.sum(axis=1), axis=0)
+	return transition_matrix
+
+def visualize_dtmc(estimated_transition_matrix, distribution, png_path):
+	import graphviz
+	dot = graphviz.Digraph()
+	for compound in estimated_transition_matrix.index:
+		# for the color of the node, use the probability in distribution (1=red, 0=transparent)
+		color = f'#{int(255 * distribution[compound]):02x}0000'
+		dot.node(str(compound), style='filled', fillcolor=color)
+	for compound in estimated_transition_matrix.index:
+		for next_compound in estimated_transition_matrix.index:
+			probability = estimated_transition_matrix.loc[[compound], [next_compound]].sum().sum()
+			if probability > 0:
+				dot.edge(str(compound), str(next_compound), label=f"{probability:.2f}")
+	dot.render(png_path)
+	print(f"Markov chain visualization saved to {png_path}")
+
+def calculate_limiting_distribution(transitions):
+	# solve $\pi \cdot P = \pi \wedge \sum_{s\in S}{\pi(s)} = 1$ to $\pi$
+	# we express this as a linear system of equations and solve it using numpy
+
+	#             p11 p12 p13
+	#             p21 p22 p23
+	#             p31 p32 p33
+	# pi1 pi2 pi3 1   1   1   = pi1 pi2 pi3
+	#
+	# pi1 p11 + pi2 p21 + pi3 p31 = pi1
+	# pi1 p12 + pi2 p22 + pi3 p32 = pi2
+	# pi1 p13 + pi2 p23 + pi3 p33 = pi3
+
+	# sympy hello world
+	# 0 = 2y+1
+	# y = sympy.Symbol('y')
+	# sympy.solve(2*y+1, y)
+
+	import sympy
+	pis = [sympy.Symbol(f'pi_{i}') for i in range(len(transitions.index))]
+	solution = sympy.solve(
+		[
+			*[
+				sum(pis[i] * transitions.loc[[transitions.index[i]], [transitions.index[j]]].sum().sum() for j in range(len(transitions.index))) - pis[i]
+				for i in range(len(transitions.index))
+			],
+			sum(pis) - 1
+		],
+		pis
+	)
 	import pdb; pdb.set_trace()
+
+	return pd.Series({
+		transitions.index[i]: solution[pis[i]]
+		for i in range(len(transitions.index))
+	})
+
+def main():
 	transitions = load_transitions()
 	logs = list(random_walks(transitions, 20, 100))
+	estimated_transition_matrix = estimate_transition_matrix(logs, transitions.index)
+
+	# histogram of the ten likeliest values in the matrix
+	import matplotlib.pyplot as plt
+	plt.hist(estimated_transition_matrix.values.flatten(), bins=100)
+	plt.savefig('transition_matrix_histogram.png')
+
+	estimated_transition_matrix = estimated_transition_matrix.fillna(0)
+
+	import pdb; pdb.set_trace()
+	limiting_distribution = calculate_limiting_distribution(estimated_transition_matrix)
+
+	visualize_dtmc(estimated_transition_matrix, limiting_distribution, 'markov_chain.pdf')
+
+	# count nans in estimated_transition_matrix
+	number_of_nans = estimated_transition_matrix.isnull().sum().sum()
+	number_of_zeros = (estimated_transition_matrix == 0).sum().sum()
 	import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
 	main()
 
 # NEXT: Learn transition matrix from logs
+
+# tell me a joke
+# once upon a time there was a programmer who wanted to learn about markov chains
+# he wrote a script to generate random walks through a markov chain
+# he then used the random walks to estimate the transition matrix of the markov chain
+# but suddenly, he realized that he had no idea how to calculate the limiting distribution of the markov chain
+# so he went to the internet and found a solution
+
+# what is a matrix determinant
+# in simple words, it's the area of a parallelogram formed by two vectors
+# to explain it to a 5-year-old, you can say that it's the area of a square formed by two vectors
+# det M :=
+
+# todos
+# - visualize 1-loops
+# - why are sums of probabilities not 1?
+# - answer questions
+# - should we use supervisory component? do we need limiting distributions?
+# mo treffen wieder?
