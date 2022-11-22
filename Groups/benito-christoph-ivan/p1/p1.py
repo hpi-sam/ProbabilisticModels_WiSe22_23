db6a8190afb7713627d81552a3069a3202b934eb
Christoph Thiede <christoph.thiede@student.hpi.de>
2022-11-22 16:14:28 +0100

    p1: always add self-loops, answer most questions, make visualization optional and extract to method, add pie charts for distribution
    Co-authored-by: Benito <benbuc@users.noreply.github.com>
    Co-authored-by: dniprocat <dniprocat@users.noreply.github.com>

--- Groups/benito-christoph-ivan/p1/p1.py
+++ Groups/benito-christoph-ivan/p1/p1.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from __future__ import annotations
 from dataclasses import dataclass
 import numpy as np
@@ -18,7 +20,16 @@ def load_transitions(file_name):
 
 	return transitions
 
-def add_self_loops(transitions):
+def add_self_loops(transitions, probability=0.5):
+	extended_transitions = transitions.copy()
+	for component in extended_transitions.index:
+		# TODO: denormalize probability (optional)
+		extended_transitions.loc[component, component] = probability
+	# normalize row-wise
+	extended_transitions = extended_transitions.div(extended_transitions.sum(axis=1), axis=0)
+	return extended_transitions
+
+def add_stuck_self_loops(transitions):
 	extended_transitions = transitions.copy()
 	for component in extended_transitions.index:
 		extended_transitions.loc[component, component] = 1 - extended_transitions.loc[component].sum()
@@ -45,7 +56,7 @@ STATES: typing.List[StateType] = ['operational', 'degraded', 'unresponsive']
 
 def generate_transitions(component_transitions):
 	proportions = pd.DataFrame(
-		[[0.6, 0.15, 0.02], [0.3, 0.5, 0.1], [0.2, 0.2, 0.1]],
+		[[0.6, 0.15, 0.02], [0.3, 0.5, 0.5], [0.2, 0.2, 0.1]],
 		index=STATES,
 		columns=STATES
 	)
@@ -136,7 +147,7 @@ def visualize_dtmc(estimated_transitions, distribution, png_path):
 		g = b = 1 - (distribution[compound] / distribution.max())
 		color = f'#{int(r * 255):02x}{int(g * 255):02x}{int(b * 255):02x}'
 		dot.node(str(compound), style='filled', fillcolor=color)
-		# TODO: improve positioning?
+		# TODO: improve positioning? (optional)
 	for compound in estimated_transitions.index:
 		for next_compound in estimated_transitions.index:
 			probability = estimated_transitions.loc[[compound], [next_compound]].sum().sum()
@@ -146,23 +157,95 @@ def visualize_dtmc(estimated_transitions, distribution, png_path):
 	dot.format = 'png'
 	dot.render(png_path)
 
-def main(completion_strategy=add_supervisory_component, anim=False):
-	component_transitions = load_transitions('../../../ProjectDescriptions/mRubis_Transition_Matrix.xlsx')
+def answer_questions(transitions, distributions):
+	# Types of traces:
+	# Normal operation:
+	#   c1/operational -> c2/operational
+	# Intermittent failure:
+	#   c1/operational -> c1/degraded -> c1/operational
+	# Systemic degradation:
+	#   c1/degraded -> c2/degraded
+	# Failure masking:
+	#   c1/unresponsive -> c2/unresponsive -> c1/operational
+	# Failure cascade:
+	#   c1/degraded -> c1/unresponsive -> c2/unresponsive
+	#   c1/degraded -> c2/degraded -> c2/unresponsive
+	import pdb; pdb.set_trace()
 
-	component_transitions = completion_strategy(component_transitions)
+	for n in [10, 20, 30]:
+		print(f"After {n} steps:")
 
-	transitions = generate_transitions(component_transitions)
-	logs = list(random_walks(transitions, 20, 100))
-	estimated_transitions = estimate_transition_matrix(logs, transitions.index)
+		print("What is the probability of seeing a failure cascade that affects only component 1 (Bid and Buy Service)?")
+		result = sum(
+			distributions[i][(component, 'degraded')] * transitions.loc[(component, 'degraded'), ('Bid and Buy Service', 'degraded')] * transitions.loc[('Bid and Buy Service', 'degraded'), ('Bid and Buy Service', 'unresponsive')]
+			for component in transitions.index.get_level_values(0).unique().difference(['Bid and Buy Service'])
+			for i in range(0, n + 1 - 2)
+		) / (n + 1 - 2)
+		print(result)
 
-	estimated_transitions = estimated_transitions.fillna(0)
+		print("What is the probability of seeing a failure cascade that affects component 2 (Item Management Service)?")
+		result = sum(
+			distributions[i][(component, 'degraded')] * (
+				transitions.loc[(component, 'degraded'), (component, 'unresponsive')] * transitions.loc[(component, 'unresponsive'), ('Item Management Service', 'unresponsive')]
+				+
+				transitions.loc[(component, 'degraded'), ('Item Management Service', 'degraded')] * transitions.loc[('Item Management Service', 'degraded'), ('Item Management Service', 'unresponsive')]
+			for component in transitions.index.get_level_values(0).unique().difference(['Item Management Service'])
+			for i in range(0, n + 1 - 2)
+		) / (n + 1 - 2)
+		print(result)
 
-	if anim:
+		print("What is the probability of seeing a failure masking?")
+		result = sum(
+			distributions[i][(component_1, 'unresponsive')] * transitions.loc[(component_1, 'unresponsive'), (component_2, 'unresponsive')] * transitions.loc[(component_2, 'unresponsive'), (component_1, 'operational')]
+			for component_1 in transitions.index.get_level_values(0).unique()
+			for component_2 in transitions.index.get_level_values(0).unique().difference([component_1])
+			for i in range(0, n + 1 - 2)
+		) / (n + 1 - 2)
+		print(result)
+
+		print("What is the probability of seeing a systemic degradation?")
+		result = sum(
+			distributions[i][(component_1, 'degraded')] * transitions.loc[(component_1, 'degraded'), (component_2, 'degraded')]
+			for component_1 in transitions.index.get_level_values(0).unique()
+			for component_2 in transitions.index.get_level_values(0).unique().difference([component_1])
+			for i in range(0, n + 1 - 1)
+		) / (n + 1 - 1)
+		print(result)
+
+		print("What is the probability of normal operation?")
+		result = sum(
+			distributions[i][(component, 'operational')]
+			for component in transitions.index.get_level_values(0).unique()
+			for i in range(0, n + 1)
+		) / (n + 1)
+		print(result)
+
+		print("What is the probability of having 1, 2, or more intermittent failures?")
+		result = sum(
+			distributions[i][(component, 'operational')] * transitions.loc[(component, 'operational'), (component, 'degraded')] * transitions.loc[(component, 'degraded'), (component, 'operational')]
+			for component in transitions.index.get_level_values(0).unique()
+			for i in range(0, n + 1 - 2)
+		) / (n + 1 - 2)
+		print(result)
+
+		print("In the case of an intermittent failure, what is the probability of a failure cascade?")
+		# TODO: Does "in the case" mean "immediately after", "at any time after", or that the failure cascade takes place in the middle of the intermittent failure?
+
+		print("In the case of a failure cascade, what is the probability of failure masking?")
+
+		print("Extra - In the case of an intermittent failure, what is the probability of failure masking?")
+		# TODO
+
+def calculate_and_visualize_limiting_distribution(transitions, visualize):
+	limiting_distributions = list(approximate_limiting_distribution_gen(transitions, 100))
+	limiting_distribution = limiting_distributions[-1]
+
+	if visualize == 'anim':
 		for (i, distribution) in enumerate(tqdm(
-			approximate_limiting_distribution_gen(estimated_transitions, 100),
+			limiting_distributions,
 			desc='Rendering limiting distribution',
 			total=100)):
-			visualize_dtmc(estimated_transitions, distribution, f'./dtmc_frame_{i}')
+			visualize_dtmc(transitions, distribution, f'./dtmc_frame_{i}')
 		# TODO: this is still very slow - took 30 minutes on my machine. run with caution!
 		import subprocess
 		subprocess.run([
@@ -181,8 +264,50 @@ def main(completion_strategy=add_supervisory_component, anim=False):
 		import glob
 		for path in glob.glob('./dtmc_frame_*.png'):
 			os.remove(path)
-	limiting_distribution = approximate_limiting_distribution(estimated_transitions, 100)
-	visualize_dtmc(estimated_transitions, limiting_distribution, f'./dtmc')
+	elif visualize == 'static':
+		visualize_dtmc(transitions, limiting_distribution, f'./dtmc')
+
+	return limiting_distribution, limiting_distributions
+
+def create_pie_charts(limiting_distribution):
+	import matplotlib.pyplot as plt
+	limiting_distribution.groupby(level=0).sum().sort_values(ascending=False).plot.pie(
+		autopct='%1.1f%%',
+		startangle=90,
+		counterclock=False,
+		pctdistance=0.8,
+		labeldistance=1.1,
+		rotatelabels=True,
+	)
+	plt.savefig('./limiting_distribution.png')
+
+	plt.clf()
+
+	limiting_distribution.groupby(level=1).sum().sort_values(ascending=False).plot.pie(
+		autopct='%1.1f%%',
+		startangle=90,
+		counterclock=False,
+		pctdistance=0.8, # pctdistance is the distance from the center of the pie to the start of the text generated by autopct
+		labeldistance=1.1
+	)
+	plt.savefig('./limiting_distribution_states.png')
+
+def main(completion_strategy=add_supervisory_component, visualize=None):
+	component_transitions = load_transitions('../../../ProjectDescriptions/mRubis_Transition_Matrix.xlsx')
+
+	component_transitions = completion_strategy(component_transitions)
+	component_transitions = add_self_loops(component_transitions)
+
+	transitions = generate_transitions(component_transitions)
+	logs = list(random_walks(transitions, 100, 100)) # TODO: cache and more
+	estimated_transitions = estimate_transition_matrix(logs, transitions.index)
+
+	estimated_transitions = estimated_transitions.fillna(0)
+
+	limiting_distribution, limiting_distributions = calculate_and_visualize_limiting_distribution(estimated_transitions, visualize)
+	create_pie_charts(limiting_distribution)
+
+	answer_questions(estimated_transitions, limiting_distributions)
 
 if __name__ == '__main__':
 	import pdb; pdb.set_trace()
@@ -198,11 +323,10 @@ if __name__ == '__main__':
 # so he went to the internet and found a solution
 
 # todos
+# - normalize animation frames
 # - answer questions
-# - chris:
+# - defer for presentation? chris:
 #   > Use this discussion of self-loops and supervisory component to solidify your understanding of the Markov Chain properties of being reducible and irreducible, periodic, which relate to being ergodic...
-# - visualize *convergence* to steady state
-#   > It could as simply as line charts of the probability of each state or something more elaborate like some cumulative regret of how much each state is, at any given moment, distant from the its steady state probability. This will also show you that certain states take longer to achieve their steady state probability, simply because they are more seldomly visited.
 # - do we want to reconsider the analytical solution?
 
 # next meeting: tue 13:00 - 18:00
