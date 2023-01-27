921df82598d7077e494de94519f304623c1827ea
Christoph Thiede <christoph.thiede@student.hpi.de>
2023-01-27 14:38:55 +0100

    add generated artifacts

--- Groups/benito-christoph-ivan/p3/charts.py
+++ Groups/benito-christoph-ivan/p3/charts.py
@@ -24,12 +24,17 @@ def plot_scores(scores_q, scores_s, title=None):
 	plt.plot(scores_q)
 	plt.plot(scores_s, color='red')
 	plt.legend(['Q-learning', 'Sarsa'])
+	# set y axis to -200 to 0
+	plt.ylim(-200, 0)
 	plt.xlabel('Episode')
 	plt.ylabel(f'Score ({title})' if title else 'Score')
 # %%
 scores_q = smooth(load_scores('scores_qlearning.csv'))
 scores_s = smooth(load_scores('scores_sarsa.csv'))
-plot_scores(scores_q, scores_s)
+plot_scores(scores_q[:5000], scores_s[:5000])
+
+# %%
+plot_scores(load_scores('scores_qlearning.csv')[:5000], load_scores('scores_sarsa.csv')[:5000])
 
 # %%
 p3.main(['qlearning', '-q', '--new-obstacle', '--shift'], 'scores_qlearning.csv')
